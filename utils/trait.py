1ba5903af83505fecfb0f793215b412d9ca8c925
whitenoise <whitenoise.ossdev@gmail.com>
2015-06-21 17:40:13 -0500

    updated Trait with bug fixes and docs

--- utils/trait.py
+++ utils/trait.py
@@ -9,61 +9,57 @@ class Trait(object):
             {'current': 0, 'base': 0, 'mod':0} but can have additional
             pieces of data added.
         static - whether or not the trait is static (current always equals max)
+        overflow - allow it to go over its max if dynamic
 
     The Trait class has the following properties:
         actual/max - the maximum possible value of the trait
                      this adds the "base" value and any "mod" values added
         current - what is the trait currently at
                   if static, this will return actual/max
-        fields - gives a list object of the trait's fields
+        fields - gives a list object of the trait's fields with types
+        base - base amount of the Trait
+        mod - modifier on the Trait, affects base
+        current - semi-automatically updating base + mod
 
     The Trait class has the following methods:
         maximize - set 'current' value to max/actual
         fill - alias for maximize
-        recover - alias for maximize
-        get_base - get the base value of the trait
-        set_base - update the base value of the trait
-        get_mod - get the modifying value of the trait
-        set_mod - set the modifying value of the trait
-        get_field(field) - get the value of a custom field
-        set_field(field, value) - set the value of a custom field
-        delete_field(field) - delete a field from the trait. cannot delete the
-            default fields (base, current, mod)
-        add_field(field, value) - add a new field to the trait
+        recover - clears any mods and then maximizes
+        clear_mods - sets mod to 0
 
     Example usages:
         Dynamic trait:
             health = Trait("Health")
-            health.set_base(100)
+            health.base = 100
             health.current # returns 0
             health.fill()
             health.current # returns 100
 
         Static trait:
             dodge_chance = Trait("Dodge Chance", static=True)
-            dodge_chance.set_base(20) (no need to fill, because it is static)
+            dodge_chance.base = 20 (no need to fill, because it is static)
             dodge_chance.current # returns 20
 
         Buff/Debuff a trait:
             This works for both static and dynamic traits.
 
             health = Trait("Health")
-            health.set_base(100)
+            health.base = 100
             health.fill()
             health.current # returns 100
-            health.set_mod(-20)
+            health.mod = -20
             health.current # returns 100 if dynamic
             health.maximize()
             health.current # returns 80
 
         Dealing with multiple modifiers:
             health = Trait("Health")
-            health.set_base(100)
-            health.set_mod(100) # we get a +100 max health buff
-            health.set_mod(health.get_mod() - 30) # we get a -30 health debuff
+            health.base = 100
+            health.mod = 100 # we get a +100 max health buff
+            health.mod = (health.mod - 30) # we get a -30 health debuff
             health.maximize()
             health.current # returns 170
-            health.set_mod(0) # clear all modifiers
+            health.clear_mods() # clear all modifiers
             health.recover()
             health.current # returns 100
     """
@@ -73,6 +69,7 @@ class Trait(object):
         base=0,
         mod=0,
         static=False,
+        overflow=False,
         **kwargs
     ):
         self.name = name.title()
@@ -80,7 +77,7 @@ class Trait(object):
         self.data = {
             'base': base,
             'mod': mod,
-            'current': base + mod
+            'current': 0
         }
         for key, value in kwargs.iteritems():
             self.data[key] = value
@@ -92,26 +89,39 @@ class Trait(object):
         return u"%s:\t\t%s" % (self.name, self.current)
 
     @property
+    def actual(self):
+        return self.data['base'] + self.data['mod']
+
+    @property
     def base(self):
         return self.data['base']
 
     @base.setter
     def base(self, amount):
-        if amount.isdigit():
+        if type(amount) in [int, float]:
             self.data['base'] = amount
         else:
             return False
 
     @property
     def current(self):
-        return self.data['current']
+        if self.static:
+            return self.actual
+        else:
+            return self.data['current']
 
     @current.setter
     def current(self, amount):
-        if amount.isdigit():
+        if type(amount) in [int, float]:
+            if self.current + amount >= self.actual and not self.overflow:
+                self.data['current'] = self.actual
             self.data['current'] = amount
         else:
             return False
+
+    @property
+    def max(self):
+        return self.actual
 
     @property
     def mod(self):
@@ -119,16 +129,14 @@ class Trait(object):
 
     @mod.setter
     def mod(self, amount):
-        if amount.isdigit():
+        if type(amount) in [int, float]:
             self.data['mod'] = amount
+            # if this is a dynamic trait, when a mod is applied
+            # we should update current immediately
+            if not self.static:
+                self.maximize()
         else:
             return False
-
-    def actual(self):
-        return self.data['base'] + self.data['mod']
-
-    def max(self):
-        return self.actual
 
     def maximize(self):
         self.current = self.actual
@@ -137,10 +145,8 @@ class Trait(object):
         self.maximize()
 
     def recover(self):
+        self.clear_mods()
         self.maximize()
 
-    def fields(self):
-        fields = []
-        for field in self.data.keys():
-            fields.append(field)
-        return fields
+    def clear_mods(self):
+        self.mod = 0
