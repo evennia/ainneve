51096843f87d34105366bbdb19b379ae4071fb5d
whitenoise <whitenoise.ossdev@gmail.com>
2015-06-22 17:35:56 -0500

    changed hardcoded value to __name__

--- plugins/ainneveraces/__init__.py
+++ plugins/ainneveraces/__init__.py
@@ -2,6 +2,6 @@ from plugins import plugger
 
 
 def load_race(race):
-    path = 'plugins.ainneveraces.' + race.lower() + '.' + race.title()
+    path = __name__ + '.' + race.lower() + '.' + race.title()
     race_class = plugger.load_plugin_class(path)
     return race_class
