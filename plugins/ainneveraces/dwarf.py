1ca1699772226f38a6aec2a421e800a586abbb86
ChrisHC <chrishc@gmx.net>
2015-06-23 04:27:40 +0200

    some preliminary defaults for the races, changed willpower to just will

--- plugins/ainneveraces/dwarf.py
+++ plugins/ainneveraces/dwarf.py
@@ -5,3 +5,10 @@ class Dwarf(Race):
     def __init__(self):
         super(Dwarf, self).__init__()
         self.name = "Dwarf"
+        self.size = "small"
+        self.limbs = []
+        self.slots = {}
+        self.foci = ['brawn', 'resilience', 'alertness']
+        self.feats = ['heat vision', 'poison resistance', 'dark vision', 'improved climb', 'fear resistance']
+        self.bonuses = {'willsave': 1}
+        self.detriments = {}
--- plugins/ainneveraces/elf.py
+++ plugins/ainneveraces/elf.py
@@ -5,3 +5,10 @@ class Elf(Race):
     def __init__(self):
         super(Elf, self).__init__()
         self.name = "Elf"
+        self.size = "medium"
+        self.limbs = []
+        self.slots = {}
+        self.foci = ['agility', 'spirit', 'alertness']
+        self.feats = ['magic resistance', 'heat vision', 'improved listen', 'sprint', 'illusion resistance' ]
+        self.bonuses = {}
+        self.detriments = {}
