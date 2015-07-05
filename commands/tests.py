479b9c34a0cab557b50936fa8a04778b8b6f88a4
George Oliver <georgeolivergo@gmail.com>
2015-07-05 14:40:17 -0700

    fixing wrong import path

--- commands/tests.py
+++ commands/tests.py
@@ -1,14 +1,10 @@
 # A trivial test for Travis
 
 from evennia.commands.default.tests import CommandTest
-from ainneve.commands.equip_commands import CmdEquip
-from ainneve.typeclasses.characters import Character
+from commands.equip_commands import CmdEquip
+from typeclasses.characters import Character
 
 class TestEquip(CommandTest):
     character_typeclass = Character
     def test_equip(self):
         self.call(CmdEquip(), "", "You have nothing in your equipment.")
-
-
-
-
