4718a273c09e4e760d2d0439d41080e0140a4629
George Oliver <georgeolivergo@gmail.com>
2015-07-07 08:16:55 -0700

    first sketch of dual archetype merge

--- world/archetypes/archetype.py
+++ world/archetypes/archetype.py
@@ -8,3 +8,11 @@ class Archetype(object):
                                'vitality': 0,
                                'magic': 0}
         self.bonuses = {}
+
+    @staticmethod
+    def make_dual(a, b):
+        dual_archetype =  Archetype()
+        dual_archetype.primary_traits = \
+            {k: (a.primary_traits[k] + b.primary_traits[k])/2
+             for k in a.primary_traits.keys() + b.primary_traits.keys()}
+        return dual_archetype
