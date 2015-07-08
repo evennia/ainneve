1bd69ab01eaa7b04b299e74ac1d827c2c68fff22
George Oliver <georgeolivergo@gmail.com>
2015-07-08 08:18:51 -0700

    refactor to simplify base template and attr names

--- world/archetypes/archetype.py
+++ world/archetypes/archetype.py
@@ -1,18 +1,21 @@
 class Archetype(object):
     def __init__(self):
-        self.primary_traits = {'strength': 0,
-                               'perception': 0,
-                               'intelligence': 0,
-                               'dexterity': 0,
-                               'charisma': 0,
-                               'vitality': 0,
-                               'magic': 0}
-        self.bonuses = {}
+        self.template = {'strength': 0,
+                         'perception': 0,
+                         'intelligence': 0,
+                         'dexterity': 0,
+                         'charisma': 0,
+                         'vitality': 0,
+                         'magic': 0}
+        self.health_die = None
+        self.health_bonus = None
 
     @staticmethod
     def make_dual(a, b):
-        dual_archetype =  Archetype()
-        dual_archetype.primary_traits = \
-            {k: (a.primary_traits[k] + b.primary_traits[k])/2
-             for k in a.primary_traits.keys() + b.primary_traits.keys()}
-        return dual_archetype
+        dual =  Archetype()
+        dual.template = \
+            {k: (a.template[k] + b.template[k])/2
+             for k in a.template.keys() + b.template.keys()}
+        dual.health_die = min(a.health_die, b.health_die)
+        dual.health_bonus = min(a.health_bonus, b.health_bonus)
+        return dual
