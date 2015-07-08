d8a9517dc10c1e8f58067a6553a7824fc7643639
George Oliver <georgeolivergo@gmail.com>
2015-07-08 08:25:32 -0700

    filled out base template with initial values

--- world/archetypes/archetype.py
+++ world/archetypes/archetype.py
@@ -6,16 +6,17 @@ class Archetype(object):
                          'dexterity': 0,
                          'charisma': 0,
                          'vitality': 0,
-                         'magic': 0}
+                         'magic': 0,
+                         'movement': 0,
+                         'power': 0}
         self.health_die = None
         self.health_bonus = None
 
     @staticmethod
     def make_dual(a, b):
         dual =  Archetype()
-        dual.template = \
-            {k: (a.template[k] + b.template[k])/2
-             for k in a.template.keys() + b.template.keys()}
+        dual.template = {k: (a.template[k] + b.template[k]) / 2
+                         for k in a.template.keys() + b.template.keys()}
         dual.health_die = min(a.health_die, b.health_die)
         dual.health_bonus = min(a.health_bonus, b.health_bonus)
         return dual
