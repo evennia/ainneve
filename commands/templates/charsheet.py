e6539709f20a602815750f374764ef31153abab9
Dan Feeney <feend78@gmail.com>
2015-11-29 00:05:45 -0600

    worked around inadvertent |- based tab in charsheet

--- commands/templates/charsheet.py
+++ commands/templates/charsheet.py
@@ -14,22 +14,22 @@ FORM = """
   |YName|n: xxxxxxxxxxxxxAxxxxxxxxxxxxxx     |YXP|n: xxCxx / xxxxDxxxx   |YLevel|n: xEx  |
   |YArchetype|n: xxxxxxxxxxxBxxxxxxxxxxx                                         |
                                        +-------------------------------------|
-  ooooooooooooooooooooooooooooooooooo  || |YPrimary Traits   |CStrength|n    : xFx  |
-  ooooooooooooooooo1ooooooooooooooooo  || ~~~~~~~~~~~~~~   |CPerception|n  : xGx  |
-  ooooooooooooooooooooooooooooooooooo  ||                  |CIntelligence|n: xHx  |
-                                       ||                  |CDexterity|n   : xIx  |
-  |YRace|n:           xxxxxxxxxxxYxxxxxx   ||                  |CCharisma|n    : xJx  |
-  |YFocus|n:          xxxxxxxxxxxZxxxxxx   ||                  |CVitality|n    : xKx  |
-  |YDescription|n                          ||                  |CMagic|n       : xLx  |
-  ~~~~~~~~~~~                          ||-------------------------------------|
-  ooooooooooooooooo2ooooooooooooooooo  || |YSave Rolls       |CFortitude|n   : xMx  |
-  ooooooooooooooooooooooooooooooooooo  || ~~~~~~~~~~       |CReflex|n      : xNx  |
-                                       ||                  |CWill|n        : xOx  |
-  |YEncumbrance|n                          ||-------------------------------------|
-  ~~~~~~~~~~~                          || |YCombat Stats     |CMelee|n       : xPx  |
-  |CCarry Weight|n:          xUxx / xVxx   || ~~~~~~~~~~~~     |CRanged|n      : xQx  |
-  |CEncumbrance Penalty|n:          xWxx   || |CPower Point      Unarmed|n     : xRx  |
-  |CMovement Points|n:              xXxx   || |CBonus|n:   xTx     |CDefense|n     : xSx  |
+  ooooooooooooooooooooooooooooooooooo  | |YPrimary Traits   |CStrength|n    : xFx  |
+  ooooooooooooooooo1ooooooooooooooooo  | ~~~~~~~~~~~~~~   |CPerception|n  : xGx  |
+  ooooooooooooooooooooooooooooooooooo  |                  |CIntelligence|n: xHx  |
+                                       |                  |CDexterity|n   : xIx  |
+  |YRace|n:           xxxxxxxxxxxYxxxxxx   |                  |CCharisma|n    : xJx  |
+  |YFocus|n:          xxxxxxxxxxxZxxxxxx   |                  |CVitality|n    : xKx  |
+  |YDescription|n                          |                  |CMagic|n       : xLx  |
+  ~~~~~~~~~~~                          | ----------------------------------- |
+  ooooooooooooooooo2ooooooooooooooooo  | |YSave Rolls       |CFortitude|n   : xMx  |
+  ooooooooooooooooooooooooooooooooooo  | ~~~~~~~~~~       |CReflex|n      : xNx  |
+                                       |                  |CWill|n        : xOx  |
+  |YEncumbrance|n                          | ----------------------------------- |
+  ~~~~~~~~~~~                          | |YCombat Stats     |CMelee|n       : xPx  |
+  |CCarry Weight|n:          xUxx / xVxx   | ~~~~~~~~~~~~     |CRanged|n      : xQx  |
+  |CEncumbrance Penalty|n:          xWxx   | |CPower Point      Unarmed|n     : xRx  |
+  |CMovement Points|n:              xXxx   | |CBonus|n:   xTx     |CDefense|n     : xSx  |
                                        +-------------------------------------|
 """
 
