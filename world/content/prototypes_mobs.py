2e9bcdbd6ecd1ae43169d1d2b0f1e36500846b99
Stefan Ludlow <ludlow.stefan@gmail.com>
2016-08-19 11:54:34 -0400

    Various npc items and such and such

--- world/content/prototypes_mobs.py
+++ world/content/prototypes_mobs.py
@@ -3,14 +3,35 @@ Prototype module containing mobs.
 """
 
 from evennia.utils import fill
-from random import randint
 from world.rulebook import d_roll
+import random
+import variables
+from variables import *
+
 
 # "traits": lambda: {'STR':2,'HP':randint(3,5)}
 #'traits': {'STR': lambda: d_roll('1d8')}
 
 SAMPLE_NPC = {
     "key": "a sample npc",
+    "aliases": ["sample", "npc"],
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "I'm a doctor Jim, not a bloody NPC",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+RAT = {
+    "sdesc": "a happy rat",
     "aliases": ["sample", "npc"],
     "tag": ["NPC"],
     "typeclass": "typeclasses.characters.NPC",
@@ -64,7 +85,25 @@ DEER = {
 }
 WOLF = {
     "key": "a lithe wolf",
-    "aliases": ["deer", "brown"],
+    "aliases": ["wolf", "lithe"],
+    "tag": ["NPC","AGGRESSIVE"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "This is a relatively sturdy looking deer.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+BEAR = {
+    "key": "a monstrous bear",
+    "aliases": ["bear", "monstrous"],
     "tag": ["NPC","AGGRESSIVE"],
     "typeclass": "typeclasses.characters.NPC",
     "desc": "This is a relatively sturdy looking deer.",
@@ -89,3 +128,5 @@ GOBLIN = {
     "traits": {'STR': lambda: d_roll('1d8'), 'DEX': lambda: d_roll('1d8')},
     "skills": {'escape': 10, 'barter': 3},
 }
+
+
