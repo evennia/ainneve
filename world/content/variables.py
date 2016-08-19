c23d07ef4d5f8b337c9c9e4d5cbd389e86e69f51
Stefan Ludlow <ludlow.stefan@gmail.com>
2016-08-19 11:56:47 -0400

    Added a variable file

---
+++ world/content/variables.py
@@ -0,0 +1,28 @@
+"""
+Variables
+"""
+
+rat_adj_1 = [
+    "brown",
+    "ochre-furred",
+    "grey",
+    "chalky-coated",
+    "charcoal-furred",
+    "sable",
+    "albino",
+    "white",
+    "red-eyed",
+    "yellow-eyed",
+    "black-eyed",
+    "glossy-eyed",
+    "dull-eyed",
+    "long-tailed",
+    "short-tailed",
+    "tailless",
+    "sharp-toothed",
+    "toothless",
+    "jagged-toothed",
+    "rotting-toothed",
+    "large-pawed",
+    "small-pawed",
+]
