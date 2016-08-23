c176139ca32241b55ac27224e0f5d907a39e55bd
Stefan Ludlow <ludlow.stefan@gmail.com>
2016-08-23 19:40:24 -0400

    Version 1 of Ainneve NPCs (#81)
    * Initial npc prototype creation
    
    * Added Rabbit mobile, testing out Lambda
    
    * Added Rabbit mobile, testing out Lambda
    
    * More Mobiles
    
    * Functioning lambda traits
    
    * Various npc items and such and such
    
    * Added a variable file
    
    * Functioning random rat descriptions. Kudos to Feend78 for sorting out all the various issues.
    
    * Update to npcs
    
    * 	"black-haired",
    	"coal-haired",
    	"ebony-haired",
    	"pumice-haired",
    	"midnight-haired",
    	"jet-haired",
    	"onyx-haired",
    	"dun-haired",
    	"dark-brown-haired",
    	"green-haired",
    	"dark-green-haired",
    	"dark-blue-haired",
    	"sorrel-haired",
    	"brown-haired",
    	"dusky-haired",
    	"crimson-haired",
    	"dark-red-haired",
    	"white-haired",
    	"scar-faced",
    	"snaggletoothed",
    	"leer-faced",
    	"sharp-toothed",
    	"wrinkle-faced",
    	"scraggly-haired",
    	"cracked-lipped",
    	"large-eared",
    	"lopsided-looking",
    	"stooped",
    	"bent-shouldered",
    	"narrow-faced",
    	"nefarious-looking",
    	"black-lipped",
    	"blue-lipped",
    	"wickedly scarred",
    	"wart-covered",
    	"saw-toothed",
    	"small-toothed",
    	"hunchbacked",
    	"triangular-faced",
    	"sneer-faced",
    	"gap-toothed",
    	"brown-toothed",
    	"yellow-toothed",
    	"wickedly-fanged",
    
    * Further NPCs and variables added.
    
    * Further NPCs and variables added.
    
    * Changed name of variables.py to sdesc_vars.py
    
    * Further NPCs and variables added.
    
    * Updated to resolve minor typos

--- world/content/prototypes_mobs.py
+++ world/content/prototypes_mobs.py
@@ -2,3 +2,152 @@
 Prototype module containing mobs.
 """
 
+from evennia.utils import fill
+from world.rulebook import d_roll
+import random
+from sdesc_vars import *
+
+### Notes
+#variable traits: 'traits': {'STR': lambda: d_roll('1d8')}
+#Icarus_: I just pushed an update to the spawner mechanism; you should be able to use callable for all fields now (keys, locations, whatever). There is also the 'exec' field where you can put executable python code. in that code you can use 'obj' to access the object just created. That way you can do e.g. exec:"obj.sdesc('sdesc_string')" to call the sdesc handler on creation.
+#    "exec": "obj.execute_cmd('say My tags include {}'.format(obj.db.tag))",
+
+
+SAMPLE_NPC = {
+    "key": "a sample npc",
+    "aliases": ["sample", "npc"],
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "I'm a doctor Jim, not a bloody NPC",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+RAT = {
+    "key":"a rat npc",
+    "sdesc": lambda: "a {} {} rat".format(random.choice(rat_adj_1),random.choice(rat_adj_2)),
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "Beware of rat poison.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+RABBIT = {
+    "key":"a rabbit npc",
+    "sdesc": "a small rabbit",
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "This is a small, fluffy white rabbit. It is likely harmless",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+DEER = {
+    "key":"a deer npc",
+    "sdesc": "a brown deer",
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "This is a relatively sturdy looking deer.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+WOLF = {
+    "key":"a wolf npc",
+    "sdesc": lambda: "a {} {} wolf".format(random.choice(wolf_adj_1),random.choice(wolf_adj_2)),
+    "tag": ["NPC","AGGRESSIVE"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "Oh look, here's a wolf. It's probably going to attempt to eat you.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+SPIDER = {
+    "key": "a spider npc",
+    "sdesc": lambda: "a {} {} spider".format(random.choice(spider_adj_1),random.choice(spider_adj_2)),
+    "tag": ["NPC","AGGRESSIVE"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "Need a hand? Well just you wait. We'll help you out, we each have eight.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+ORC = {
+    "key": "an orc npc",
+    "sdesc": lambda: "a {} {} orc".format(random.choice(orc_adj_1),random.choice(orc_adj_2)),
+    "tag": ["NPC","AGGRESSIVE"],
+    "exec": "obj.execute_cmd('say My tags include {}'.format(obj.db.tag))",
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "An intellectual creature at heart, the orc is a misunderstood beast who simply desires to be understood and loved. It expresses this love through extreme violence.",
+    "traits": {'STR': 5, 'DEX': 5, 'PER': 5, 'CHA': 5, 'INT': 5, 'VIT': 5,
+               'BM': 5, 'WM': 5, 'MAG': 10,
+               'REFL': 5, 'FORT': 5, 'WILL': 5, 'ENC': 50,
+               'HP': 5, 'MV': 5, 'SP': 5, 'LV': 1, 'ACT': 5,
+               'ATKM': 5, 'DEF': 5, 'ATKR': 5, 'PP': 5, 'ATKU': 5},
+    "skills": {'escape': 1, 'climb': 1, 'jump': 1,
+               'lockpick': 1, 'listen': 1, 'sense': 1,
+               'appraise': 1, 'medicine': 1, 'survival': 1,
+               'balance': 1, 'sneak': 1, 'throwing': 1,
+               'animal': 1, 'barter': 1, 'leadership': 1},
+}
+
+
+GOBLIN = {
+    "key": "a tiny little goblin",
+    "aliases": ["goblin", "tiny"],
+    "tag": ["NPC"],
+    "typeclass": "typeclasses.characters.NPC",
+    "desc": "It's a little green goblin. It wants your love and affection",
+    "traits": {'STR': lambda: d_roll('1d8'), 'DEX': lambda: d_roll('1d8')},
+    "skills": {'escape': 10, 'barter': 3},
+}
+
+
