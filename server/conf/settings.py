751b5aa81766f963aa079469289f671658d5221d
= <dgisser@gmail.com>
2017-10-11 12:57:12 -0400

    Consistent settings file with update

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -1,18 +1,27 @@
 """
 Evennia settings file.
+
 The available options are found in the default settings file found
 here:
+
 {settings_default}
+
 Remember:
+
 Don't copy more from the default file than you actually intend to
 change; this will make sure that you don't overload upstream updates
 unnecessarily.
+
 When changing a setting requiring a file system path (like
 path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
 your game folder and the Evennia library folders respectively. Python
 paths (path.to.module) should be given relative to the game's root
 folder (typeclasses.foo) whereas paths within the Evennia library
 needs to be given explicitly (evennia.foo).
+
+If you want to share your game dir, including its settings, you can
+put secret game- or server-specific settings in secret_settings.py.
+
 """
 
 # Use the defaults from Evennia unless explicitly overridden
@@ -25,8 +34,29 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "Ainneve"
 
+# Server ports. If enabled and marked as "visible", the port
+# should be visible to the outside world on a production server.
+# Note that there are many more options available beyond these.
+
 IRC_ENABLED = True
 IDLE_TIMEOUT = 86400
+# Telnet ports. Visible.
+TELNET_ENABLED = True
+TELNET_PORTS = [4000]
+# (proxy, internal). Only proxy should be visible.
+WEBSERVER_ENABLED = True
+WEBSERVER_PORTS = [(4001, 4002)]
+# Telnet+SSL ports, for supporting clients. Visible.
+SSL_ENABLED = False
+SSL_PORTS = [4003]
+# SSH client ports. Requires crypto lib. Visible.
+SSH_ENABLED = False
+SSH_PORTS = [4004]
+# Websocket-client port. Visible.
+WEBSOCKET_CLIENT_ENABLED = True
+WEBSOCKET_CLIENT_PORT = 4005
+# Internal Server-Portal port. Not visible.
+AMP_PORT = 4006
 
 ######################################################################
 # Django web features
