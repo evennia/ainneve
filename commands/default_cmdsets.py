2db6ede323555fd64ee86fe493d57207231672c0
ChrisLR <arzhul@gmail.com>
2022-08-31 16:22:31 -0400

    Added two WIP basic debug commands

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -17,6 +17,7 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 from evennia import default_cmds
 from commands import equip, chartraits, room_exit, chargen, \
                         building, skills, combat
+from commands.debug import DebugCmdSet
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
@@ -41,6 +42,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(building.AinneveBuildingCmdSet())
         self.add(skills.SkillCmdSet())
         self.add(combat.InitCombatCmdSet())
+        self.add(DebugCmdSet())
         
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
