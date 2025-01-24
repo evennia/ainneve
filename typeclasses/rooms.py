3dd2ed6737eb9f0aa444b684a134b22d1314d778
ChrisLR <arzhul@gmail.com>
2025-01-24 15:49:23 -0500

    Fix args

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -85,7 +85,7 @@ class OverworldRoom(wilderness.WildernessRoom, Room):
 
         return tile_str
 
-    def at_object_receive(self, source_location, moved_obj, **kwargs):
+    def at_object_receive(self, moved_obj, source_location, **kwargs):
         return super().at_object_receive(source_location=source_location, moved_obj=moved_obj)
 
 
